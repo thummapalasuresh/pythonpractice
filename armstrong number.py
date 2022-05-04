f9fe76e00719a6583fd756b92bed90b1cef76b3f
thummapalasuresh <104911425+thummapalasuresh@users.noreply.github.com>
2022-05-04 19:46:37 +0530

    Add files via upload

---
+++ armstrong number.py
@@ -0,0 +1,10 @@
+##n = int(input("enter the numbetr"))
+##tostring = str(n)
+##sum=0
+##for i in tostring:
+##    sum = sum+ int(i)* int(i)* int(i)
+##    print (sum)
+##    if (n==sum):
+##        print("success")
+
+

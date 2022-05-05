550b08325fba27715d2e9086ea8572108207d86f
thummapalasuresh <104911425+thummapalasuresh@users.noreply.github.com>
2022-05-05 16:14:58 +0530

    Add files via upload

---
+++ python functions.py
@@ -0,0 +1,98 @@
+##def myfun(name,place):
+##    if isinstance(name,str) and isinstance(place, str):
+##        print ("hello user", name, "from", place)
+##        print ("welome to netzwerk academy")
+##myfun(name="suri", place="australia")
+##
+
+##def passed(*names):
+##    print(names)
+##   print (names)
+##passed("dhoni", "suresh")
+
+##def passed(**names):
+##    print(names)
+##    print (names)
+##passed(names="dhoni", names="suresh")
+
+##def studentmarks(eng, math, s_name):
+##
+##    total= eng+math
+##    return total
+##
+##output= studentmarks(50,46,"dhoni")
+##print(output)
+
+##def studentmarks(eng, math, s_name):
+##
+##    total= eng+math
+##    return total, s_name
+##
+##output= studentmarks(50,46,"dhoni")
+##print(output)
+##if output[0]> 100:
+##    print("1")
+##else:
+##    print("0")
+
+##def studentmarks(eng, math, s_name):
+##
+##    total= eng+math
+##    return total, s_name
+##
+##output= studentmarks(50,46,"dhoni")
+##print(output)
+##if output[0]< 100:
+##    print("1")
+##else:
+##    print("0")
+
+
+##count = 10
+##def fun():
+##    count=100
+##    print (count)
+##    
+##print(count)
+##fun()
+##print(count)
+
+
+##count = 10
+##def fun():
+##    global count
+##    count=100
+##    print (count)
+##    
+##print(count)
+##fun()
+##print (count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

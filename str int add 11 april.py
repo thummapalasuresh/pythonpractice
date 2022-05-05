6096f10f571a3e15958364ee69a2c066ed226940
thummapalasuresh <104911425+thummapalasuresh@users.noreply.github.com>
2022-05-05 16:14:24 +0530

    Add files via upload

---
+++ str int add 11 april.py
@@ -0,0 +1,30 @@
+name="dhoni"
+age= 43
+data=" my captain " + name +  " plays cricket at age of " + str(age)
+print (data)
+data=" my captain  %s   plays cricket at age of %d" %(name,age)  
+print (data)
+data=" my captain {} plays cricket at age of {}" .format(name,age)
+print (data)
+data= f" my captain {name} plays cricket at age of {age}" 
+print (data)
+var = "latha"
+print (var[2])
+print (var[-2])
+print (var[2:7])
+print (var[::2])
+print (var[::4])
+print (var[::-4])
+
+var = input("enter name")
+print(var)
+print (type(var))
+
+
+var = int(input("enter number"))
+print(var)
+print (type(var))
+
+
+
+
